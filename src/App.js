0da372010ce2edf433ca3b53920052b743b5ba59
AnthonyJee <17538252697@163.com>
2023-07-07 19:26:37 +0800

    res

--- src/App.js
+++ src/App.js
@@ -11,16 +11,28 @@ export default function App() {
     const arr = [...JSON.parse(JSON.stringify(list))];
     arr.push(value);
     setlist(arr);
-    const REACT_APP_API_KEY = process.env.REACT_APP_API_KEY;
+    const OPENAI_API_KEY = process.env.REACT_APP_API_KEY;
     axios
-      .get("http://123.56.14.21:3001/chat", {
-        params: { value, REACT_APP_API_KEY },
-      })
+      .post(
+        "https://api.openai.com/v1/chat/completions",
+        {
+          model: "gpt-3.5-turbo-16k-0613",
+          messages: [{ role: "user", content: value }],
+          temperature: 0.7,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${OPENAI_API_KEY}`,
+          },
+        }
+      )
       .then((res) => {
-        arr.push(res.data);
+        arr.push(res.data.choices);
         setdisabled(false);
         setlist(arr);
-        console.log(res);
+
+        console.log(arr);
       })
       .catch((error) => {
         // 处理错误
@@ -28,6 +40,7 @@ export default function App() {
         console.log(error);
       });
   };
+
   return (
     <div>
       <div className="chat-container">
@@ -42,7 +55,7 @@ export default function App() {
             ) : (
               <div className="chat-message" key={index}>
                 <span className="user">ChatGpt:</span>
-                <span className="content">{item[0]?.message.content}</span>
+                <span className="content">{item[0].message.content}</span>
               </div>
             );
           })}
